22045558fb959455380dcaf772a5efef955e99f4
wagnerdk <18245675+wagnerdk@users.noreply.github.com>
2018-10-30 23:18:11 +0100

    Add an example spring boot REST application

---
+++ smart-energy-system/src/main/java/com/github/smartenergysystem/SmartEnergySystemApplication.java
@@ -0,0 +1,12 @@
+package com.github.smartenergysystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartEnergySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartEnergySystemApplication.class, args);
+	}
+}
