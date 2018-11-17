cd145c56a87c3423fd30ae4c35e2876923f6e172
wagnerdk <18245675+wagnerdk@users.noreply.github.com>
2018-11-17 13:16:11 +0100

    Remove os specific file separators

--- smart-energy-system/src/main/java/com/github/smartenergysystem/weather/DatabaseConfig.java
+++ smart-energy-system/src/main/java/com/github/smartenergysystem/weather/DatabaseConfig.java
@@ -1,6 +1,7 @@
 package com.github.smartenergysystem.weather;
 
 
+import java.io.FileNotFoundException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import javax.sql.DataSource;
@@ -8,18 +9,24 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
 @Configuration
 public class DatabaseConfig {
 		
 	@Bean  
-    public DataSource dataSource() {  
+    public DataSource dataSource() throws FileNotFoundException {  
         DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();  
         dataSourceBuilder.driverClassName("org.sqlite.JDBC");
         //Get path to DB
         Path currentPath = Paths.get(System.getProperty("user.dir"));
         Path basePath = currentPath.getParent().getParent();
         //TODO put it in config
-        dataSourceBuilder.url("jdbc:sqlite:"+basePath.toString()+"\\WeatherCollector\\weather.db"); 
+        Path subPath = Paths.get("WeatherCollector", "weather.db");
+        Path fullPath = basePath.resolve(subPath);
+        if(!fullPath.toFile().exists()) {
+        	throw new FileNotFoundException("There is no weather.db in this path:"+ fullPath);
+        }
+        dataSourceBuilder.url("jdbc:sqlite:"+fullPath); 
         return dataSourceBuilder.build();  
     }
 
